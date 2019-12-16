9fbcecaa2736c0aa91ad32a371198cb01df0bbfb
杨凯彬 <ykb553@163.com>
2019-12-16 20:25:38 +0800

    luttuce

--- HuGo/content/_sidebar.md
+++ HuGo/content/_sidebar.md
@@ -4,7 +4,7 @@ headless: true
 
 - **Client**
   - [Jedis Pool]({{< relref "/docs/Client/Jedis-Pool.md" >}})
-  - [*Lettuce*]({{< relref "/docs/Client/Lettuce.md" >}})
+  - [Lettuce]({{< relref "/docs/Client/Lettuce.md" >}})
 - **监控**
   - [INFO]({{< relref "/docs/Monitor/INFO.md" >}})
   - [Redis Manager 监控指标]({{< relref "/docs/Monitor/Redis-Manager-Metrics.md" >}})
--- redis-lettuce/src/main/java/xyz/kail/demo/redis/lettuce/Main.java
+++ redis-lettuce/src/main/java/xyz/kail/demo/redis/lettuce/Main.java
@@ -1,24 +1,15 @@
-# Lettuce
+package xyz.kail.demo.redis.lettuce;
 
-> 官网： https://lettuce.io/
->
-> 官方文档： https://lettuce.io/docs/getting-started.html
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
+import io.lettuce.core.api.reactive.RedisReactiveCommands;
+import io.lettuce.core.api.sync.RedisCommands;
 
+public class Main {
 
+    public static void main(String[] args) {
 
-Spring Boot 2 的 Redis 客户端 从 `Jedis` 换成了 `Lettuce`，所以注意到了它。
-
-Lettuce 是一个可伸缩的线程安全的 Redis 客户端，支持同步、异步和响应式模式。
-
-**多个线程可以共享一个连接实例，而不必担心多线程并发问题**。
-
-它基于优秀 Netty NIO 框架构建，支持 Redis 的高级功能，如 Sentinel，集群，流水线，自动重新连接和 Redis 数据模型。
-
-
-
-## 示例代码
-
-```bash
 RedisClient redisClient = RedisClient.create("redis://password@localhost:6379/0");
 
 try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
@@ -38,5 +29,6 @@ try (StatefulRedisConnection<String, String> connection = redisClient.connect())
 
 
 redisClient.shutdown();
-```
+    }
 
+}
