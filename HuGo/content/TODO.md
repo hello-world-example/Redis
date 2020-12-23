73e6c3c0f181f06f10cd813c187de20aaccdb315
kaibin.yang <ykb553@163.com>
2020-12-23 15:21:54 +0800

    good

---
+++ HuGo/content/TODO.md
@@ -0,0 +1,5 @@
+## TODO
+
+- RedLock
+    - https://www.cnblogs.com/ironPhoenix/p/6048467.html
+- as
--- redis-redisson/src/main/java/xyz/kail/demo/redis/redisson/Main.java
+++ redis-redisson/src/main/java/xyz/kail/demo/redis/redisson/Main.java
@@ -1,8 +1,18 @@
 package xyz.kail.demo.redis.redisson;
 
+import org.redisson.Redisson;
+import org.redisson.api.RAtomicLong;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
 public class Main {
 
     public static void main(String[] args) {
+        Config config=new Config();
+        config.useSingleServer().setAddress("");
+
+        final RedissonClient redisson = Redisson.create(config);
+
 
     }
 
